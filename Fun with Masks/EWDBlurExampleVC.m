b31c3e085d5dbffd7752a7fc0e0e0ce098a6c902
Evan Davis <evan.davis@bottlerocketapps.com>
2013-02-09 18:19:15 -0600

    added some commenting to explain what's going on

--- Fun with Masks/EWDBlurExampleVC.m
+++ Fun with Masks/EWDBlurExampleVC.m
@@ -47,22 +47,29 @@
     [scrollView setContentSize:CGSizeMake(CGRectGetWidth(bigImage.bounds), CGRectGetHeight(bigImage.bounds) - 49.0f)];
     [self.view addSubview:scrollView];
     
+    //create our blurred image
     CIContext *context = [CIContext contextWithOptions:nil];
     CIImage *inputImage = [CIImage imageWithCGImage:theImage.CGImage];
+    
+    //setting up Gaussian Blur (we could use one of many filters offered by Core Image)
     CIFilter *filter = [CIFilter filterWithName:@"CIGaussianBlur"];
     [filter setValue:inputImage forKey:kCIInputImageKey];
     [filter setValue:[NSNumber numberWithFloat:15.0f] forKey:@"inputRadius"];
     CIImage *result = [filter valueForKey:kCIOutputImageKey];
+    //CIGaussianBlur has a tendency to shrink the image a little, this ensures it matches up exactly to the bounds of our original image 
     CGImageRef cgImage = [context createCGImage:result fromRect:[inputImage extent]];
     
+    //add our blurred image to the scrollview
     UIImageView *blurView = [[UIImageView alloc] initWithFrame:bigImage.bounds];
     blurView.contentMode = UIViewContentModeScaleAspectFill;
     blurView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
     blurView.image = [UIImage imageWithCGImage:cgImage];
     [scrollView addSubview:blurView];
     
+    //this is the frame we offset in scrollViewDidScroll
     self.buttonFrame = CGRectMake(20.0f, 160.0f, CGRectGetWidth(self.view.bounds) - 40.0f, 40.0f);
     
+    //setting up button
     self.testButton = [UIButton buttonWithType:UIButtonTypeCustom];
     self.testButton.frame = self.buttonFrame;
     self.testButton.layer.borderWidth = 2.0f;
@@ -73,6 +80,7 @@
     [self.testButton setTitleColor:[UIColor grayColor] forState:UIControlStateHighlighted];
     [scrollView addSubview:self.testButton];
     
+    //setting up corresponding mask for the button. 
     self.maskLayer = [CALayer layer];
     self.maskLayer.frame = self.buttonFrame;
     self.maskLayer.cornerRadius = self.testButton.layer.cornerRadius;
@@ -106,6 +114,7 @@
     buttonFrame.origin.x += offset.x;
     buttonFrame.origin.y += offset.y;
     self.testButton.frame = buttonFrame;
+    //without the CATransaction the testButton's frame setting is actually slighty animated, appearing to give it a delay as we scroll around
     [CATransaction begin];
     [CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions];
     self.maskLayer.frame = buttonFrame;
--- Fun with Masks/EWDGradientExampleVC.m
+++ Fun with Masks/EWDGradientExampleVC.m
@@ -26,6 +26,7 @@
 
 - (void)loadView
 {
+    //initial view setup
     self.view = [[UIView alloc] initWithFrame:[UIScreen mainScreen].applicationFrame];
     self.view.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
     self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"pw_maze_white"]];
@@ -44,23 +45,7 @@
     UIView *containerView = [[UIView alloc] initWithFrame:self.view.bounds];
     containerView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
     [containerView addSubview:scrollView];
-    
     [self.view addSubview:containerView];
-    
-    CAGradientLayer *maskLayer = [CAGradientLayer layer];
-    maskLayer.startPoint = CGPointMake(0.3f, 1.0f);
-    maskLayer.endPoint = CGPointMake(0.9f, 0.0f);
-    UIColor *outerColor = [UIColor colorWithWhite:1.0 alpha:0.0];
-    UIColor *innerColor = [UIColor colorWithWhite:1.0 alpha:1.0];
-    maskLayer.colors = @[(id)outerColor.CGColor, (id)outerColor.CGColor, (id)innerColor.CGColor, (id)innerColor.CGColor];
-    
-    maskLayer.locations = @[@0.0, @0.05, @0.5, @1.0f];
-    maskLayer.bounds = CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds));
-    maskLayer.anchorPoint = CGPointZero;
-    
-    containerView.layer.mask = maskLayer;
-    
-    
     
     UILabel *testLabel = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, CGRectGetHeight(self.view.bounds) - 30.0f, 60.0f, 20.0f)];
     testLabel.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin;
@@ -68,6 +53,30 @@
     testLabel.text = @"Howdy!";
     [self.view addSubview:testLabel];
     
+    //creating our gradient mask
+    CAGradientLayer *maskLayer = [CAGradientLayer layer];
+    
+    //this is the anchor point for our gradient, in our case top left. setting it in the middle (.5, .5) will produce a radial gradient. our startPoint and endPoints are based off the anchorPoint
+    maskLayer.anchorPoint = CGPointZero;
+    
+    //The line between these two points is the line our gradient uses as a guide
+    //starts in bottom left
+    maskLayer.startPoint = CGPointMake(0.0f, 1.0f);
+    //ends in top right
+    maskLayer.endPoint = CGPointMake(1.f, 0.0f);
+
+    
+    //setting our colors - since this is a mask the color itself is irrelevant - all that matters is the alpha. A clear color will completely hide the layer we're masking, an alpha of 1.0 will completely show the masked view.
+    UIColor *outerColor = [UIColor colorWithWhite:1.0 alpha:0.0];
+    UIColor *innerColor = [UIColor colorWithWhite:1.0 alpha:1.0];
+    
+    //an array of colors that dictatates the gradient(s)
+    maskLayer.colors = @[(id)outerColor.CGColor, (id)outerColor.CGColor, (id)innerColor.CGColor, (id)innerColor.CGColor];
+    //these are percentage points along the line defined by our startPoint and endPoint and correspond to our colors array. The gradient will shift between the colors between these percentage points.
+    maskLayer.locations = @[@0.0, @0.05, @0.5, @1.0f];
+    
+    maskLayer.bounds = CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds));
+    containerView.layer.mask = maskLayer; 
 }
 
 - (void)viewDidLoad
