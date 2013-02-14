3d6d48e3f36c842c16c594e4783f19b576a9fde1
Evan Davis <evan.davis@bottlerocketapps.com>
2013-02-13 20:33:49 -0600

    switched to using .xib files to setup views

--- Fun with Masks/EWDAnimationExampleVC.m
+++ Fun with Masks/EWDAnimationExampleVC.m
@@ -11,9 +11,13 @@
 
 @interface EWDAnimationExampleVC ()
 
-@property (nonatomic, strong) UIView *maskView;
+@property (nonatomic, weak) IBOutlet UIView *maskView;
+@property (nonatomic, weak) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (nonatomic, strong) CAShapeLayer *maskLayer;
 @property (nonatomic, assign) BOOL isMaskMoved;
+
+- (IBAction)toggleMask:(id)sender;
 
 @end
 
@@ -27,45 +31,12 @@
     }
     return self;
 }
-
-- (void)loadView
-{
-    self.view = [[UIView alloc] initWithFrame:[UIScreen mainScreen].applicationFrame];
-    self.view.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"pw_maze_white"]];
-    
-    
-    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
-    scrollView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-
-    UIImage *theImage = [UIImage imageNamed:@"sample_profile_bg.jpg"];
-    UIImageView *bigImage = [[UIImageView alloc] initWithImage:theImage];
-    bigImage.image = theImage;
-    bigImage.contentMode = UIViewContentModeScaleAspectFill;
-    bigImage.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-    [scrollView addSubview:bigImage];
-    
-    [scrollView setContentSize:CGSizeMake(CGRectGetWidth(bigImage.bounds), CGRectGetHeight(bigImage.bounds) - 49.0f)];
-    [self.view addSubview:scrollView];
-    
-    self.maskView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, CGRectGetWidth(self.view.bounds), 100.0f)];
-    self.maskView.backgroundColor = [UIColor grayColor];
-    [self.view addSubview:self.maskView];
-    
-    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    button.frame = CGRectMake(0.0f, 0.0f, 100.0f, 44.0f);
-    [button setTitle:@"Move Mask" forState:UIControlStateNormal];
-    [button addTarget:self action:@selector(toggleMask:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:button];
-    button.center = self.view.center;
-    
-    [self toggleMask:nil];
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"pw_maze_white"]];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -74,12 +45,20 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)toggleMask:(id)sender
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    self.scrollView.contentSize = self.imageView.bounds.size;
+    [self toggleMask:nil];
+}
+
+- (IBAction)toggleMask:(id)sender
 {
     self.isMaskMoved = !self.isMaskMoved;
     CGFloat width = self.maskView.layer.frame.size.width;
     CGFloat height = self.maskView.layer.frame.size.height;
     
+    //Create path that defines the edges of our masking layer
     CGMutablePathRef path = CGPathCreateMutable();
     
     CGPathMoveToPoint(path, NULL, 0, 0);
@@ -94,16 +73,16 @@
     CGPathAddLineToPoint(path, NULL, 0, height);
     CGPathCloseSubpath(path);
     
-    
+    //if no mask, create it
     if (!self.maskLayer)
     {
         self.maskLayer = [[CAShapeLayer alloc] init];
         self.maskLayer.frame = self.maskView.layer.bounds;
         self.maskLayer.fillColor = [[UIColor blackColor] CGColor];
         self.maskLayer.path = path;
-        
         self.maskView.layer.mask = self.maskLayer;
     }
+    //animate our mask to the new path 
     else
     {
         CABasicAnimation *anim = [CABasicAnimation animationWithKeyPath:@"path"];
--- Fun with Masks/EWDBlurExampleVC.m
+++ Fun with Masks/EWDBlurExampleVC.m
@@ -10,9 +10,14 @@
 #import <QuartzCore/QuartzCore.h>
 @interface EWDBlurExampleVC () <UIScrollViewDelegate>
 
-@property (nonatomic, strong) UIButton *testButton;
 @property (nonatomic, strong) CALayer *maskLayer;
 @property (nonatomic, assign) CGRect buttonFrame;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIImageView *originalImageView;
+@property (weak, nonatomic) IBOutlet UIButton *testButton;
+@property (weak, nonatomic) IBOutlet UIImageView *blurView;
+
+- (IBAction)toggleMask:(id)sender;
 
 @end
 
@@ -27,25 +32,57 @@
     return self;
 }
 
-- (void)loadView
+- (void)viewDidLoad
 {
-    self.view = [[UIView alloc] initWithFrame:[UIScreen mainScreen].applicationFrame];
-    self.view.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    [super viewDidLoad];
     self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"pw_maze_white"]];
+    [self setupBlurredImage];
     
-    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
-    scrollView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-    scrollView.delegate = self;
     
+    
+    //this is the frame we offset in scrollViewDidScroll
+    self.buttonFrame = [self.view convertRect:self.testButton.frame toView:self.blurView];
+    
+    //setting up button
+    self.testButton.layer.borderWidth = 2.0f;
+    self.testButton.layer.borderColor = [UIColor blackColor].CGColor;
+    self.testButton.layer.cornerRadius = CGRectGetHeight(self.testButton.bounds) / 2;
+    
+    //setting up corresponding mask for the button.
+    self.maskLayer = [CALayer layer];
+    self.maskLayer.frame = self.buttonFrame;
+    self.maskLayer.cornerRadius = self.testButton.layer.cornerRadius;
+    self.maskLayer.backgroundColor = [UIColor blackColor].CGColor;
+    self.blurView.layer.mask = self.maskLayer;
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    self.scrollView.contentSize = self.originalImageView.bounds.size;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - mask manipulation
+
+- (IBAction)toggleMask:(id)sender
+{
+    if (self.blurView.layer.mask)
+        self.blurView.layer.mask = nil;
+    else
+        self.blurView.layer.mask = self.maskLayer;
+}
+
+#pragma mark - Image Blurring
+
+- (void)setupBlurredImage
+{
     UIImage *theImage = [UIImage imageNamed:@"sample_profile_bg.jpg"];
-    
-    UIImageView *bigImage = [[UIImageView alloc] initWithImage:theImage];
-    bigImage.contentMode = UIViewContentModeScaleAspectFill;
-    bigImage.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-    
-    [scrollView addSubview:bigImage];
-    [scrollView setContentSize:CGSizeMake(CGRectGetWidth(bigImage.bounds), CGRectGetHeight(bigImage.bounds) - 49.0f)];
-    [self.view addSubview:scrollView];
     
     //create our blurred image
     CIContext *context = [CIContext contextWithOptions:nil];
@@ -56,52 +93,11 @@
     [filter setValue:inputImage forKey:kCIInputImageKey];
     [filter setValue:[NSNumber numberWithFloat:15.0f] forKey:@"inputRadius"];
     CIImage *result = [filter valueForKey:kCIOutputImageKey];
-    //CIGaussianBlur has a tendency to shrink the image a little, this ensures it matches up exactly to the bounds of our original image 
+    //CIGaussianBlur has a tendency to shrink the image a little, this ensures it matches up exactly to the bounds of our original image
     CGImageRef cgImage = [context createCGImage:result fromRect:[inputImage extent]];
     
     //add our blurred image to the scrollview
-    UIImageView *blurView = [[UIImageView alloc] initWithFrame:bigImage.bounds];
-    blurView.contentMode = UIViewContentModeScaleAspectFill;
-    blurView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-    blurView.image = [UIImage imageWithCGImage:cgImage];
-    [scrollView addSubview:blurView];
-    
-    //this is the frame we offset in scrollViewDidScroll
-    self.buttonFrame = CGRectMake(20.0f, 160.0f, CGRectGetWidth(self.view.bounds) - 40.0f, 40.0f);
-    
-    //setting up button
-    self.testButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    self.testButton.frame = self.buttonFrame;
-    self.testButton.layer.borderWidth = 2.0f;
-    self.testButton.layer.borderColor = [UIColor blackColor].CGColor;
-    self.testButton.layer.cornerRadius = CGRectGetHeight(self.testButton.bounds) / 2;
-    [self.testButton setTitle:@"Howdy!" forState:UIControlStateNormal];
-    [self.testButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [self.testButton setTitleColor:[UIColor grayColor] forState:UIControlStateHighlighted];
-    [scrollView addSubview:self.testButton];
-    
-    //setting up corresponding mask for the button. 
-    self.maskLayer = [CALayer layer];
-    self.maskLayer.frame = self.buttonFrame;
-    self.maskLayer.cornerRadius = self.testButton.layer.cornerRadius;
-    self.maskLayer.backgroundColor = [UIColor blackColor].CGColor;
-    
-    blurView.layer.mask = self.maskLayer;
-    
-    
-    
-}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    self.blurView.image = [UIImage imageWithCGImage:cgImage];
 }
 
 #pragma mark - UIScrollViewDelegate
@@ -113,8 +109,8 @@
     CGRect buttonFrame = self.buttonFrame;
     buttonFrame.origin.x += offset.x;
     buttonFrame.origin.y += offset.y;
-    self.testButton.frame = buttonFrame;
-    //without the CATransaction the testButton's frame setting is actually slighty animated, appearing to give it a delay as we scroll around
+    NSLog(@"button's frame: %f, %f, %f, %f", buttonFrame.origin.x, buttonFrame.origin.y, buttonFrame.size.width, buttonFrame.size.height);
+    //without the CATransaction the mask's frame setting is actually slighty animated, appearing to give it a delay as we scroll around
     [CATransaction begin];
     [CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions];
     self.maskLayer.frame = buttonFrame;
